28e1486c7994c805d020c83d900fab22ea5cdb4c
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-20 10:38:39 +0200

    'update'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <script setup>
-window.addEventListener("keypress", (event)=> {
-  let display= document.querySelector('h1')
-  let projectile= document.querySelector('#ball')
-  let leftPaddle=document.querySelector('.left')
-  let rightPaddle=document.querySelector('.right')
-  let width=window.innerWidth
-  let height=window.innerHeight
-  let key=event.key
-if(key== 'w'){
-leftPaddle.style=`
-top:${leftPaddle.getBoundingClientRect().y +1 +'px' }
-`
-}
-else if(key== 's'){
-  leftPaddle.style=`
-top:${leftPaddle.getBoundingClientRect().y -1 +'px' }
-`
-}
-}
-  );
-function sendUserData(){
-let startTime= Date.now()
-}
-  
+import { ref, onMounted } from "vue";
+
+const el = ref();
+
+onMounted(() => {
+  let display = document.querySelector("h1");
+  let projectile = document.querySelector("#ball");
+  let leftPaddle = document.querySelector(".left");
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let initialDirection = 0;
+  let displacement = "1px";
+  let rightPaddle = document.querySelector(".right");
+  let startTime = Date.now();
+
+  window.addEventListener("keypress", (event) => {
+    let key = event.key;
+    if (key == "q" && leftPaddle.getBoundingClientRect().y > 0) {
+      leftPaddle.style = `
+      top:${leftPaddle.getBoundingClientRect().y + displacement};
+      `;
+    } else if (
+      key == "a" &&
+      leftPaddle.getBoundingClientRect().bottom < height
+    ) {
+      leftPaddle.style = `
+      top:${leftPaddle.getBoundingClientRect().bottom + displacement};
+      `;
+    } else if (key == "p" && rightPaddle.getBoundingClientRect().y > 0) {
+      rightPaddle.style = `
+    top:${rightPaddle.getBoundingClientRect().y + displacement};
+    `;
+    } else if (
+      key == "l" &&
+      rightPaddle.getBoundingClientRect().bottom < height
+    ) {
+      rightPaddle.style = `
+    top:${rightPaddle.getBoundingClientRect().bottom + displacement};
+    `;
+    } else if (key == " ") {
+      fetch("https://yesno.wtf/api")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.answer == "no") {
+            initialDirection = -1;
+          } else {
+            initialDirection = 1;
+          }
+        });
+    }
+  });
+  function sendUserData() {
+    if (projectile.getBoundingClientRect().bottom < height && projectile.getBoundingClientRect().y > 0) {
+      projectile.style = `
+        top:${projectile.getBoundingClientRect().bottom + displacement};
+        `;
+      }
+      console.log(projectile.getBoundingClientRect().bottom, width);
+    setTimeout(sendUserData, 1000);
+  }
+  sendUserData();
+});
 </script>
 
 <template>
@@ -34,48 +72,59 @@ let startTime= Date.now()
   </div>
 </template>
 
-<style >
-*{
+<style>
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
 }
 ::-webkit-scrollbar {
-display: none;
+  display: none;
 }
-html{
+html {
+  position: fixed;
+  top: 0;
+  left: 0;
+  margin: 1em;
   width: 100vw;
   height: 100vh;
 }
-.bodyContainer{
+.bodyContainer {
   width: 100vw;
   height: 100vh;
 }
-.paddle{
+.paddle {
   width: 1em;
   height: 7vw;
   background-color: red;
 }
-.left{
+.left {
   position: fixed;
   left: 2%;
-  top :50%;
-  transform: translate(-50%,-50%);
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
-.right{
+.right {
   position: fixed;
   right: 2%;
-  top :50%;
-  transform: translate(-50%,-50%);
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
-#ball{
+#ball {
   height: 3em;
-aspect-ratio: 1;
-border-radius: 50%;
-  position :fixed;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  position: fixed;
   left: 50%;
-  top :50%;
-  transform: translate(-50%,-50%);
-  background-image: radial-gradient(circle, #efefef, #c6c6c6, #9e9e9e, #797979, #555555);
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-image: radial-gradient(
+    circle,
+    #efefef,
+    #c6c6c6,
+    #9e9e9e,
+    #797979,
+    #555555
+  );
 }
 </style>
